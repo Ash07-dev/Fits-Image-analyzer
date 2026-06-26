92c631b905ea68b390844e66108fcff764b89ea9
Ashwini Kumar Sethi <118682546+Ash07-dev@users.noreply.github.com>
2026-06-26 16:17:05 +0530

    Modified Main.py
    Updated the FITS file and changed colormap for images.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
 from astropy.visualization import ImageNormalize,LogStretch, ZScaleInterval
 
-fits_file = fits.open("horsehead.fits") #Using the fits file
+fits_file = fits.open("NGC121.fits") #Using the fits file
 # print(fits_file)
 
 image_data = fits_file[0].data   # Checking the data is it there
@@ -12,17 +12,18 @@ image_data = fits_file[0].data   # Checking the data is it there
 # Removes Noise
 norm = ImageNormalize(image_data, interval=ZScaleInterval(), stretch=LogStretch())
 
+#Changing the cmap changes the output colour of the image
 plt.figure()
-plt.imshow(image_data, origin="lower", norm = norm, cmap="copper")
+plt.imshow(image_data, origin="upper", norm = norm, cmap="YlOrRd")
 plt.colorbar()
-plt.savefig('main.png', dpi=150)
+plt.savefig('Images/main_02.png', dpi=150)
 plt.close()
 
 # To get a section in the image 
 section1 = image_data[2000:2250, 1500:2500]
 
 plt.figure()
-plt.imshow(section1, origin="lower", norm = LogNorm(), cmap = "Greys")
+plt.imshow(section1, origin="lower", norm = LogNorm(), cmap = "grey")
 plt.colorbar()
-plt.savefig('main_section.png', dpi=150)
+plt.savefig('Images/main_section02.png', dpi=150)
 plt.close()
